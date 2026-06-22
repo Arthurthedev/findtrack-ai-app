745b67cdece9e12c7e72db985ebd36f617747231
Arthur Moraes <arthur.m.thurler@gmail.com>
2026-06-21 21:46:54 -0300

    feat: centralize auth layout structure

---
+++ src/app/(auth)/_components/auth-layout.tsx
@@ -0,0 +1,48 @@
+import MoneyLogo from "../../../assets/money-logo.png";
+import Image from "next/image";
+import Link from "next/link";
+
+interface AuthLayoutProps {
+    title: string;
+    description: string;
+    children: React.ReactNode;
+    footerText: string;
+    footerLinkText: string;
+    footerHref: string;
+}
+
+export const AuthLayout = ({
+    children,
+    description,
+    footerHref,
+    footerLinkText,
+    footerText,
+    title,
+}: AuthLayoutProps) => {
+    return (
+        <section className="h-screen flex items-center justify-center">
+            <div className="bg-[#181818] w-full max-w-md p-8 rounded-2xl text-white">
+                <div className="flex flex-col items-center mb-8">
+                    <div className="bg-[#9333EA] h-16 w-16 flex items-center justify-center rounded-2xl mb-6">
+                        <Image
+                            src={MoneyLogo}
+                            alt="ícone da página de autenticação"
+                        />
+                    </div>
+                    <h1 className="text-white text-2xl font-bold mb-2">
+                        {title}
+                    </h1>
+                    <p className="text-[#9F9FA9] text-sm">{description}</p>
+                </div>
+                {children}
+
+                <div className="mt-10 text-center">
+                    <p className="text-[#9F9FA9] text-sm">
+                        {footerText}
+                        <Link href={footerHref} className="ml-1 text-[#9333EA] font-semibold hover:underline">{footerLinkText}</Link>
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
