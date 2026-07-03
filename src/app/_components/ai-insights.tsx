8a734320bf76b99c8cdc9faf113293c798028465
Arthur Moraes <arthur.m.thurler@gmail.com>
2026-07-02 21:45:18 -0300

    feat: create AI-Insights component

---
+++ src/app/_components/ai-insights.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import starIcon from "@/src/assets/ai-insights/star-icon.png";
+import bulbIcon from "@/src/assets/ai-insights/bulb-icon.png";
+import insightsIcon from "@/src/assets/ai-insights/insights-icon.png";
+import refreshIcon from "@/src/assets/ai-insights/refresh-icon.png";
+
+export function AiInsights() {
+    return (
+        <div className="text-white space-y-6">
+            <div className="flex items-center gap-2">
+                <Image src={starIcon} alt="star icon" />
+                <h3 className="text-xl font-bold">Insights com IA</h3>
+            </div>
+
+            <div className="bg-[#161B26] p-6 rounded-2xl flex items-center gap-4">
+                <div className="bg-[#A855F7]/20 p-4 rounded-xl">
+                    <Image src={insightsIcon} alt="insights icon" />
+                </div>
+
+                <div>
+                    <p>Categoria com maior gasto</p>
+                    <p className="text-sm text-[#94A3B8] ">Alimentação: <span className="text-[#9333EA] ml-1 font-semibold">R$ 42,00</span></p>
+                </div>
+            </div>
+
+            <div className="bg-[#10B9810D] p-6 rounded-2xl flex items-center gap-4">
+                <div className="bg-[#10B98133] p-4 rounded-xl">
+                    <Image src={bulbIcon} alt="bulb icon" />
+                </div>
+
+                <div>
+                    <p>Sugestão de economia</p>
+                    <p className="text-sm text-[#CBD5E1]">
+                        Para economizar em alimentação, cozinhe mais em casa. Planeje as refeições e leve marmita.
+                    </p>
+                </div>
+            </div>
+
+            <button className="flex items-center justify-center w-full gap-3 border-2 border-dashed border-[#1E293B] py-4 rounded-2xl hover:border-[#9333EA] cursor-pointer">
+                <Image src={refreshIcon} alt="refresh icon" />
+                <span>Atualizar análise</span>
+            </button>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@ import BalanceCard from "./_components/balance-card";
 import FinancialMetricCard from "./_components/financial-metric-card";
 import Header from "./_components/header";
 import Sidebar from "./_components/sidebar";
-import { ChartCard } from "./_components/chart-card";
+import ChartCard from "./_components/chart-card";
+import { AiInsights } from "./_components/ai-insights";
 
 export default function Home() {
     return (
@@ -13,25 +14,29 @@ export default function Home() {
                 <main className="p-8 space-y-8">
                     <section className="grid lg:grid-cols-3 grid-cols-1 gap-6">
                         <div className="lg:col-span-2 col-span-1">
-                        <BalanceCard
-                            balance={1000}
-                            revenues={500}
-                            expenses={500}
-                        />
+                            <BalanceCard
+                                balance={1000}
+                                revenues={500}
+                                expenses={500}
+                            />
                         </div>
                         <FinancialMetricCard
                             economy="+12%"
                             economyValue={120.0}
                         />
                     </section>
-                    <section className="grid lg:grid-cols-2 grid-cols-1 gap-6">
-
-                        <ChartCard
-                            depositsTotal={1000}
-                            expensesTotal={500}
-                            investmentsTotal={500}
-                            balance={1000}
-                        />
+                    <section className="flex gap-8">
+                        <div className="flex-1">
+                            <ChartCard
+                                depositsTotal={1000}
+                                expensesTotal={500}
+                                investmentsTotal={500}
+                                balance={1000}
+                            />
+                        </div>
+                        <div className="flex-1">
+                            <AiInsights />
+                        </div>
                     </section>
                 </main>
             </div>
