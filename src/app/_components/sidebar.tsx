b756d80395ec1f90f81422d4ea3c150b2f3fc33b
Arthur Moraes <arthur.m.thurler@gmail.com>
2026-07-28 22:42:50 -0300

    feat: add transactions list component

--- src/app/_components/sidebar.tsx
+++ src/app/_components/sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 
@@ -5,6 +6,8 @@ import dashboardIcon from "@/src/assets/sidebar/dashboard-icon.png";
 import transactionsIcon from "@/src/assets/sidebar/transactions-icon.png";
 import logo from "@/src/assets/sidebar/logo-icon.png";
 import { Logout } from "./logout";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -12,6 +15,11 @@ const inter = Inter({
 });
 
 export default function Sidebar() {
+    const pathname = usePathname();
+
+    const isDashboard = pathname === "/";
+    const isTransactions = pathname === "/transactions";
+
     return (
         <aside
             className={`${inter.className} flex w-64 flex-col border-r border-[#1E293B] bg-[#0F111A] text-[#F1F5F9]`}
@@ -27,29 +35,29 @@ export default function Sidebar() {
             </div>
 
             <nav className="flex flex-1 flex-col gap-2 px-4 py-4">
-                <a
-                    href="#"
-                    className="flex w-full items-center gap-3 rounded-xl bg-[#9333EA] px-4 py-3 text-white"
+                <Link
+                    href="/"
+                    className={`flex w-full items-center gap-3 rounded-xl ${isDashboard ? "bg-[#9333EA] text-white" : "text-white"} px-4 py-3`}
                 >
                     <Image src={dashboardIcon} alt="icone de dashboard" />
                     <span className="text-base font-medium leading-normal">
                         Dashboard
                     </span>
-                </a>
+                </Link>
 
-                <a
-                    href="#"
-                    className="flex w-full items-center gap-3 rounded-xl px-4 py-3 text-[#94A3B8]"
+                <Link
+                    href="/transactions"
+                    className={`flex w-full items-center gap-3 rounded-xl ${isTransactions ? "bg-[#9333EA] text-white" : "text-[#94A3B8]"} px-4 py-3`}
                 >
                     <Image src={transactionsIcon} alt="icone de transações" />
                     <span className="text-base font-medium leading-normal">
                         Transações
                     </span>
-                </a>
+                </Link>
             </nav>
 
             <div className="border-t border-[#1E293B] px-6 py-6">
-                <Logout/>
+                <Logout />
             </div>
         </aside>
     );
